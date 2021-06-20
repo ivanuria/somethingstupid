daa8162fceff32145ba5ca280228a75f7c90dde1
ivanuria <ivan.uria@gmail.com>
2021-06-20 22:17:58 +0200

    Making it less pythonic

--- tries/mapslices.go
+++ tries/mapslices.go
@@ -1,8 +1,8 @@
 package tries
 
-func Mapslices() slice {
-  items := []map{}
-  items.append(map[string]interface{}{"hola": "Patatas", "nada": 1})
-  items.append(map[string]interface{}{"hola": 2, "menos": "cachopo"})
+func Mapslices() map[string]interface{} {
+  items := []map[string]interface{}{}
+  append(items, map[string]interface{}{"hola": "Patatas", "nada": 1})
+  append(items, map[string]interface{}{"hola": 2, "menos": "cachopo"})
   return items
 }
